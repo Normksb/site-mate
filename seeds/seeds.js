f0d1f0b20f682fef925acf0bd5ddbfe194d6da7a
Norman Bernard <nksb414@gmail.com>
2021-06-15 15:29:09 +0800

    updated seeds.js to include EmployeeSchedule

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,9 +1,10 @@
 const sequelize = require('../config/connection');
-const {Employee, Site, Weeks, Schedule} = require('../models');
+const {Employee, Site, Weeks, Schedule, EmployeeSchedule} = require('../models');
 const testEmployee = require('./test_employees.json');
 const testSite = require('./test_sites.json');
 const testWeeks = require('./test_weeks.json');
 const testSchedule = require('./test_schedule.json');
+const testEmployeeSchedule = require('./test_employee_schedule.json');
 
 
 const seedDatabase = async () => {
@@ -17,6 +18,8 @@ const seedDatabase = async () => {
     await Weeks.bulkCreate(testWeeks, {
     })
     await Schedule.bulkCreate(testSchedule, {
+    })
+    await EmployeeSchedule.bulkCreate(testEmployeeSchedule, {
     })
     process.exit(0)
 }
