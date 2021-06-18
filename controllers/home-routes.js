2362ef61041adbaf1ca9157be6b8d69ad10f9755
Yuri Kitchin <ykitchin@gmail.com>
2021-06-18 18:37:56 +0800

    add employee and site to scheudle routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -49,6 +49,10 @@ router.get("/schedule", async (req, res) => {
         res.redirect('/');
     }
     try {
+        const employeeData = await Employee.findAll()
+        const employee = employeeData.map((employee) => employee.get({plain:true}))
+        const siteData = await Site.findAll()
+        const site = siteData.map((site) => site.get({plain:true}))
         const scheduleData = await EmployeeSchedule.findAll({include: [
             {
                 
@@ -77,35 +81,17 @@ router.get("/schedule", async (req, res) => {
         })
         console.log("this is scheudule", schedule)
 
-
-
-        // const finalSchedule = []
-     
-        // for (let i = 0; i < schedule.length; i++) {
-        //     const element = schedule[i];
-        //     const elementTwo = schedule[i++];
-        //    if (element.site_name === elementTwo.site_name && element.week_date === elementTwo.week_date) {
-        //     finalSchedule.push(element.site_name)
-        // } 
-        // }
-        
-        // console.log(finalSchedule)
-        
-        // console.log("AAAAAAAAAAAAA", schedule)
-        // console.log("BBBBBBBBBBBBBB", schedule[0].schedule.Employee.first_name + ' ' + schedule[0].schedule.Employee.last_name)
         res.render("schedule", {
-            schedule
-            // loggedIn: req.session.loggedIn,
-            // siteId: schedule.site_id,
-            // site_name: schedule.Schedule.site.site_name,
-            // week_date: schedule.Schedule.week_date,
-            // name: schedule.Employee.first_name + ' ' + schedule.Employee.last_name,
+            schedule,
+            employee,
+            site
         })
         
     } catch (error) {
         console.error(error.message)
     } 
 });
+
 
 
 module.exports = router;
