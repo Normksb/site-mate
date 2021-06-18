57f518a2efbd8e52ee5cf03f26269934ab5463d6
Yuri Kitchin <ykitchin@gmail.com>
2021-06-18 19:22:49 +0800

    add event handler to submit new roster

--- public/js/scheduleScript.js
+++ public/js/scheduleScript.js
@@ -16,3 +16,43 @@ function showAddSched() {
     scheduleModal.classList.remove("display");
    
   }
+
+  //handle modal form
+  const newRosterHandler = async (event) => {
+    event.preventDefault();
+    const employeeData = document.querySelectorAll('input[name="employee"]:checked');
+    let employeeArray = [];
+    employeeData.forEach((Data) => {
+        employeeArray.push(Data.value)
+    });
+
+
+    const week = document.querySelector("#week").value.trim();
+    const site = document.querySelector("#site").value.trim();
+    
+    // console.log("here is employee", employee)
+    // console.log("week is", week)
+    // console.log("here is site", site)
+    console.log(employeeArray.toString())
+   
+    // if (first_name && last_name && email && password) {
+    //   const response = await fetch("/api/users", {
+    //     method: "POST",
+    //     body: JSON.stringify({ first_name, last_name, email, password }),
+    //     headers: { "Content-Type": "application/json" },
+    //   });
+  
+    //   if (response.ok) {
+    //     document.location.replace("/");
+    //   } else {
+    //     alert("Failed to sign up.");
+    //   }
+    // }
+  };
+
+
+
+
+  document
+  .querySelector('.submitBtn')
+  .addEventListener('click', newRosterHandler);
